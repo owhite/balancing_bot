76e3c99ea13b0f2074b5c47dd89e76f6160c4c84
owhite <smouldering.dog@gmail.com>
2017-03-31 09:13:19 -0400

    first commit

---
+++ test/src/variables.h
@@ -0,0 +1,85 @@
+#include "definitions.h"
+
+// Set initial input parameters
+enum Ascale {
+AFS_2G = 0,
+AFS_4G,
+AFS_8G,
+AFS_16G
+};
+
+enum Gscale {
+  GFS_250DPS = 0,
+  GFS_500DPS,
+  GFS_1000DPS,
+  GFS_2000DPS
+};
+
+enum Mscale {
+  MFS_14BITS = 0, // 0.6 mG per LSB
+  MFS_16BITS      // 0.15 mG per LSB
+};
+
+uint8_t  power = 140;
+uint8_t  direction = 1;
+uint8_t  pauseToggle = 0;
+uint8_t  reportToggle = 0;
+
+uint8_t  blinkNormal = 1000;
+uint8_t  blinkTilted = 1;
+uint8_t  blinkOn = 1;
+uint32_t blinkDelta = 0;
+uint32_t blinkInterval = blinkTilted; 
+uint32_t blinkNow;
+
+double kp, ki, kd;
+double position=0, output=0, target=0;
+
+int wireFailures = 0;
+int wireAttempts = 0;
+int wireReportCount = 10000;
+int state = 0;
+
+String inputString = "";         // a string to hold incoming data
+
+// Specify sensor full scale
+uint8_t Gscale = GFS_250DPS;
+uint8_t Ascale = AFS_2G;
+uint8_t Mscale = MFS_16BITS; // Choose either 14-bit or 16-bit magnetometer resolution
+uint8_t Mmode = 0x02;        // 2 for 8 Hz
+float aRes, gRes, mRes;      // scale resolutions per LSB for the sensors
+  
+// Pin definitions
+int intPin = 12;  // These can be changed, 2 and 3 are the Arduinos ext int pins
+int adoPin = 8;
+int myLed = 13;
+
+int16_t accelCount[3];  // Stores the 16-bit signed accelerometer sensor output
+int16_t gyroCount[3];   // Stores the 16-bit signed gyro sensor output
+int16_t magCount[3];    // Stores the 16-bit signed magnetometer sensor output
+float magCalibration[3] = {0, 0, 0}, magbias[3] = {0, 0, 0};  // Factory mag calibration and mag bias
+float gyroBias[3] = {0, 0, 0}, accelBias[3] = {0, 0, 0};      // Bias corrections for gyro and accelerometer
+int16_t tempCount;      // temperature raw count output
+float   temperature;    // Stores the real internal chip temperature in degrees Celsius
+float   SelfTest[6];    // holds results of gyro and accelerometer self test
+
+// global constants for 9 DoF fusion and AHRS (Attitude and Heading Reference System)
+float GyroMeasError = PI * (40.0f / 180.0f);   // gyroscope measurement error in rads/s (start at 40 deg/s)
+float GyroMeasDrift = PI * (0.0f  / 180.0f);   // gyroscope measurement drift in rad/s/s (start at 0.0 deg/s/s)
+float beta = sqrt(3.0f / 4.0f) * GyroMeasError;   // compute beta
+float zeta = sqrt(3.0f / 4.0f) * GyroMeasDrift;   // compute zeta, the other free parameter in the Madgwick scheme 
+#define Kp 2.0f * 5.0f // Mahony filter and fusion params, Kp for proportional feedback, Ki for integral
+#define Ki 0.0f
+
+uint32_t delt_t = 0; // used to control display output rate
+uint32_t count = 0, sumCount = 0; // used to control display output rate
+float pitch, yaw, roll;
+float deltat = 0.0f, sum = 0.0f;        // integration interval for both filter schemes
+uint32_t lastUpdate = 0, firstUpdate = 0; // used to calculate integration interval
+uint32_t Now = 0;        // used to calculate integration interval
+
+float ax, ay, az, gx, gy, gz, mx, my, mz; // variables to hold latest sensor data values 
+float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};    // vector to hold quaternion
+float eInt[3] = {0.0f, 0.0f, 0.0f};       // vector to hold integral error for Mahony method
+
+
